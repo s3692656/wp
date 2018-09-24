4d8cb666ca0e87d04b90e12273d4c9705a544182
s3692656 <s3692656@student.rmit.edu.au>
2018-09-24 16:00:58 +1000

    pages

---
+++ a2/products.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <meta name="description" content="A and V services">
+   
+    <title> A 'n V Student Services </title>
+       
+    <!-- fontello.css -->
+    <link rel="stylesheet" type="text/css" href="css/fontello.css">
+    <!-- Style CSS -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    
+</head>
+
+<body>
+     
+  <div class="topbar">
+       <h4 id="top"> 
+<i style="font-size:24px" class="fa">&#xf095;</i>
++61452***6**
+<i style="font-size:24px" class="fa">&#xf0e0;</i>
+ruhi.nar*****@gmail.com
+</h4>
+      
+    </div>
+    <!-- /.topbar -->
+    <div class="header-classic">
+        <!-- header classic -->
+        
+            
+                <a href="index.html" class=""><img src="images/logo.png" alt="our logo " height= "80" width= "200"><img src="images/services.png" alt="student services" height= "80" width = "200"></a>
+                
+           <div id="navigation" class="navigation">
+                        <!-- navigation -->
+                      <ul>
+                            <li><a href="index.html">Home</a>
+                                    
+                                           
+                            <li><a href="coaching-list-page.html">Coaching</a>
+                              <ul>
+                                  <li><a href="coaching-ielts.html">IELTS</a></li>
+                                    <li><a href="coaching-pte.html">PTE</a></li>
+                                  </ul>
+                            </li>
+                            <li><a href="visa-immigration-student.html">Visas</a>
+                                <ul>
+                                    <li><a href="visa-immigration-student.html">Student Visa</a></li>
+                                    <li><a href="visa-immigration-tourist.html">Tourist Visa</a></li>
+                                </ul>
+                            </li>
+                             <li><a href="bachelor.html">Admissions </a>
+                                <ul>
+                                    <li><a href="bachelor.html">Undergarduate</a></li>
+                                    <li><a href="masters.html">Postgraduate</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="aboutus.html">About us </a>
+                                
+                            </li><li><a href="aboutus.html">
+							  LogIn</a>
+                                
+                            </li>
+                                                                       
+                        </ul>
+                    </div>
+                </div>
+         
+    
+    
+    <!-- /.header classic -->
+   <div class="slanting-pattern"></div>
+    <!-- /.header classic -->
+    <div class="page-header">
+        <div class="container">
+                
+                    <h1 class="page-title">English test coaching</h1>
+                    <p class="page-description"> We provide best coaching. Take unlimited classes at reasonable price. </p>
+              
+        </div>
+    </div>
+    <div class="page-breadcrumb">
+        <!-- page breadcrumb -->
+        <div class="container">
+            
+        
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="index-3.html">Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Coaching</li>
+                        </ol>
+               
+        </div>
+    </div>
+
+    <div class="content">
+        <div class="container">
+           <center>
+                
+                    <div class="coaching-card">
+                        <!-- coaching card -->
+                        <div class="coaching-card-img zoomimg">
+                            <a href="coaching-ielts.html"><img src="images/ielts.jpg" alt="" class="img-fluid" height="450" width ="450"></a>
+                        </div>
+                        <div class="coaching-card-body">
+                            <h2 class="coaching-card-title"><a href="coaching-ielts.html" class="title">IELTS Coaching</a></h2>
+                            <p>Want to go with IELTS?</p>
+                            <a href="coaching-ielts.html" >Read More</a>
+                        </div>
+                    </div>
+                    <!-- /.coaching card -->
+                
+                
+                    <div class="coaching-card">
+                        <!-- coaching card -->
+                        <div class="coaching-card-img zoomimg">
+                            <a href="coaching-pte.html"><img src="images/PTE.png" alt="" class="img-fluid" height="450" width="450"></a>
+                        </div>
+                        <div class="coaching-card-body">
+                            <h2 class="coaching-card-title"><a href="coaching-pte.html" class="title">PTE Coaching</a></h2>
+                            <p> Want to go with PTE?</p>
+                            <a href="coaching-pte.html" >Read More</a>
+                        </div>
+                    </div>
+                    <!-- /.coaching card -->
+            </center>
+        </div>
+    </div>
+    <div class="slanting-pattern"></div>
+       <div class="widget widget-quote-form bg-yellow">
+                            <center>
+                            <h3 class="form-title">Book An Appointment </h3>
+                            <p class="form-text">Find out your options for visa by completing a free online assessment.</p>
+                            <form method="post" action="https://titan.csit.rmit.edu.au/~e54061/wp/processing.php
+">
+                               
+                                <!-- Text input-->
+                                <div class="form-group">
+                                    <label class="control-label sr-only" for="yourname">Your Name</label>
+                                    <div class="">
+                                        <input id="yourname" name="yourname" type="text" placeholder="Name" class="form-control" required="">
+                                    </div>
+                                </div>
+                                <!-- Text input-->
+                                <div class="form-group">
+                                    <label class="control-label sr-only" for="email">Email</label>
+                                    <div class="">
+                                        <input id="email" name="email" type="email" placeholder="Email" class="form-control" required="">
+                                    </div>
+                                </div>
+                                <!-- Text input-->
+                                <div class="form-group">
+                                    <label class="control-label sr-only" for="mobileno">Number</label>
+                                    <div class="">
+                                        <input id="numberOfMeetings" name="numberOfMeetings" type="number" min=0  placeholder="numberOfMeetings" class="form-control" required="">
+                                    </div>
+                                </div>
+                                  <div class="form-group">
+                                    <label for="selectvisa" class="sr-only">Select Visa</label>
+                                    <select class="form-control" id="selectvisa" name="selectvisa">
+                                        <option value="Select Visa">Select Visa</option>
+                                        <option value="Students Visa">Students Visa</option>
+                                        <option value="Business Visa">Business Visa</option>
+                                        <option value="Family Visa">Family Visa</option>
+                                        <option value="Travel Visa">Travel Visa </option>
+                                        <option value="Work Visa">Work Visa </option>
+                                        <option value="Visitor Visa">Visitor Visa </option>
+                                        <option value="Migrate Visa">Migrate Visa </option>
+                                        <option value="PR Visa">PR Visa</option>
+                                    </select>
+                                </div>
+                                <!-- Textarea -->
+                                <div class="form-group">
+                                    <label class="control-label sr-only" for="message">Message</label>
+                                    <div class="">
+                                        <textarea class="form-control" id="message" name="message" rows="4" placeholder="Message"></textarea>
+                                    </div>
+                                </div>
+                                <button type="submit" class="btn btn-default btn-lg btn-block">Book My Free Assessment</button>
+                            </form>
+                                </center>
+                        </div>
+                                        <div class="slanting-pattern"></div>
+
+  <!-- /. News section -->
+    <div class="footer">
+        <!-- Footer -->
+            
+            <div class="grid-container" >
+                 <div class="item1" >
+                   <!-- <div class="widget-footer"> -->
+                        <h3 class="widget-title">Country</h3>
+                        <ul class="listnone">
+                            <li>Canada</li>
+                            <li>United States</li>
+                            <li>United Kingdom</li>
+                            <li>Australia</li>
+                            <li>New Zealand</li>
+                            <li>Singapore</li>
+                            <li>South Africa</li>
+                            <li>Germany</li>
+                        </ul>
+                    </div>
+             <!--   </div>  -->
+                                <div class="slanting-pattern"></div>
+
+               <div class="item2">
+                  <!--  <div class="widget-footer"> -->
+                        <h3 class="widget-title">Contact Us</h3>
+                        <b> My office</b>
+                        <address>12 migration road
+                            <br> Abc, VIC 3000</address>
+                        <p>1800 *** ****
+                           </p>
+                        <p><a href="#">Schedule a Meeting</a>
+                            <br>
+                            <a href="#">Talk to our Expert</a></p>
+                    </div>
+                                <div class="slanting-pattern"></div>
+
+       <!--       </div> -->
+                <div class="item3" >
+               
+                        <h3 class="widget-title">Connect</h3>
+                        <ul class="listnone">
+                            <li><a href="">Facebook</a></li>
+                            <li><a href=""> Twitter</a></li>
+                            <li><a href=""> Instagram</a></li>
+                            <li><a href=""> Youtube</a></li>
+                            <li><a href=""> Linked In</a></li>
+                        </ul>
+                    </div>
+                                <div class="slanting-pattern"></div>
+
+                </div>
+           
+    </div>
+    <!-- /.footer -->
+    <div class="tiny-footer">
+        <!-- tiny footer -->
+        <div class="container">
+               
+                    <p>RUHANI s3692656 © 2018 <a href="">AnV </a></p>
+                
+            
+        </div>
+    </div>
+    <!-- /.tiny footer -->
+    
+    
+</body>
+
+</html>
